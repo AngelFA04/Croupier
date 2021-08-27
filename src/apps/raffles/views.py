7cdb4f442caa0997a0d79054007c91fde8e45185
Ángel Francisco Flores Ayala <angelfa@getfairplay.com>
2021-08-27 07:59:21 -0500

    Fix error in signup of home link

--- src/apps/raffles/views.py
+++ src/apps/raffles/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from raffles.forms import RaffleCreateForm
 
-class RaffleCreateView(CreateView):
+
+class RaffleCreateView(LoginRequiredMixin, CreateView):
+    login_url = "login"
     form_class = RaffleCreateForm
     template_name = "raffles/create_raffle.html"
     success_url = "/organizers/welcome"
 
     def get_initial(self):
         """Add the organizer to the form initial data."""
-        self.initial["organizer"] = self.request.user.organizer 
+        self.initial["organizer"] = self.request.user.organizer
         return self.initial
 
 
-
-class RaffleListView(TemplateView):
+class RaffleListView(LoginRequiredMixin, TemplateView):
     template_name = "raffles/list_raffles.html"
 
     def get(self, request, *args, **kwargs):
@@ -23,17 +24,20 @@ class RaffleListView(TemplateView):
         context["raffles"] = request.user.organizer.raffles.all()
         return self.render_to_response(context)
 
-class RaffleDetailView(TemplateView):
+
+class RaffleDetailView(LoginRequiredMixin, TemplateView):
     template_name = "raffles/detail_raffle.html"
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
-    
+
+
 class RaffleFinishView(TemplateView):
     """
     View used to finish a raffle. And send the results to the raffle owner.
     """
+
     template_name = "raffles/finish_raffle.html"
 
     def get(self, request, *args, **kwargs):
