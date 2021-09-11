bd853ae07f970fbc83465e28bcf29cfd41584e62
Ángel Francisco Flores Ayala <angelfa@getfairplay.com>
2021-09-11 00:23:13 -0500

    Started view to show public organizer profiles

--- apps/organizers/urls.py
+++ apps/organizers/urls.py
@@ -7,6 +7,9 @@ from apps import organizers
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     # path("login/", views.LoginView.as_view(), name="login"),
-    path("welcome", views.welcome, name="welcome"),
-    # path("profiles/<organizer_id:str>", views.OrganizersView)
+    path("welcome/", views.welcome, name="welcome"),
+    path("me/", views.OrganizerProfileView.as_view(), name="profile"),
+    path(
+        "<str:pk>/", views.OrganizerPublicProfileView.as_view(), name="public_profile"
+    ),
 ]
--- apps/organizers/views.py
+++ apps/organizers/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from organizers.forms import SignupForm
+from organizers.models import OrganizerModel
 
 # from django.views.generic import TemplateView
 
@@ -43,3 +47,26 @@ def welcome(request):
     # TODO Pass in the context if the user is already logged in
     context = {}
     return render(request, "organizers/welcome.html", context=context)
+
+
+class OrganizerProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "raffles/profile.html"
+    login_url = "/users/login"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context["raffles"] = request.user.organizer.raffles.all()
+        return self.render_to_response(context)
+
+
+class OrganizerPublicProfileView(DetailView):
+    model = OrganizerModel
+    template_name = "organizers/profile_public.html"
+
+    def get_context_data(self, **kwargs):
+        organizer = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context["user"] = context["object"].user
+        context["raffles"] = organizer.raffles.all()
+
+        return context
